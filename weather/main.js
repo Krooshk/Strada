08f5b24713c5dce4b9f3c2ea49ebfcbf0bbc6439
Krooshk <kirill.Krush@mail.ru>
2022-11-04 16:11:16 +0300

    feat: finish now and deatils tabs

--- weather/main.js
+++ weather/main.js
@@ -1,28 +1,72 @@
-let input = document.querySelector('input');
-const tabNow = {
-    CITY: document.querySelector('.city-now'),
-    ICON: document.querySelector('.icon-weather img'),
-    TEMP: document.querySelector('.temperature-tab1'),
+import { INPUT, tabNow, locations, tabDetails } from '/Strada/weather/view.js';
+let addedLocations = ["Amur", "Samara", "Moscow", "Tokyo"];
+const apiKey = "111836d36d452866f627599b193d2401";
+const serverUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+INPUT.addEventListener("keyup", sentName);
+getInfo("Moscow");
+tabNow.SHAPE.addEventListener("click", () => {
+    let name = tabNow.CITY.textContent;
+    if (!addedLocations.includes(name)) {
+        addedLocations.push(name);
+        createCity(name);
+        console.log(addedLocations);
+    }
+});
+
+addedLocations.forEach(locationName => {
+    createCity(locationName);
+});
+
+
+
+
+function createCity(locationName) {
+    let location = document.createElement('p');
+    location.innerHTML = `<span>${locationName}</span><img src="./assets/close-icon-45.svg" alt="close">`;
+    locations.append(location);
+    location.querySelector('img').addEventListener("click", deleteCity);
+    location.childNodes[0].addEventListener("click", () => getInfo(locationName));
+}
+
+function deleteCity() {
+    let name = this.closest("p").textContent;
+    addedLocations = addedLocations.filter(el => el !== name);
+    this.closest("p").remove();
+    console.log(addedLocations);
 }
 
 
-input.addEventListener("keyup", sentName);
 
 function sentName(event) {
     if (event.keyCode === 13) {
-        const cityName = input.value;
-        const apiKey = "111836d36d452866f627599b193d2401";
-        const serverUrl = "https://api.openweathermap.org/data/2.5/weather";
-        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-        let response = fetch(url);
-        let json = response.then((response) => response.json());
-        json.then((result) => {
-                tabNow.ICON.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`;
-                tabNow.TEMP.textContent = `${Math.round(result.main.temp)}°`;
-                tabNow.CITY.textContent = cityName;
-                input.value = "";
-            })
-            .catch(err => alert(err));
-
+        const cityName = INPUT.value;
+        getInfo(cityName);
     }
+}
+
+function getInfo(cityName) {
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+    let response = fetch(url);
+    let json = response.then((response) => response.json());
+    json.then((result) => {
+            console.log(result);
+            tabNow.ICON.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`;
+            tabNow.TEMP.textContent = `${Math.round(result.main.temp)}°`;
+            tabNow.CITY.textContent = cityName;
+            tabDetails.CITY.textContent = cityName;
+            tabDetails.TEMP.textContent = `${Math.round(result.main.temp)}`;
+            tabDetails.FEEL_LIKE.textContent = `${Math.round(result.main.feels_like)}`;
+            tabDetails.WEATHER.textContent = result.weather[0].main;
+            tabDetails.SUNSET.textContent = `${new Date(1000*result.sys.sunset).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})}`;
+            tabDetails.SUNRISE.textContent = `${new Date(1000*result.sys.sunrise).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})}`;
+
+            INPUT.value = "";
+        })
+        .catch(err => alert(err));
+}
+
+
+for (let el in tabDetails) {
+    console.log(el);
 }
---
+++ weather/view.js
@@ -0,0 +1,16 @@
+export let INPUT = document.querySelector('input');
+export let tabNow = {
+    CITY: document.querySelector('.city-now'),
+    ICON: document.querySelector('.icon-weather img'),
+    TEMP: document.querySelector('.temperature-tab1'),
+    SHAPE: document.querySelector('.shape'),
+};
+export let locations = document.querySelector('.locations');
+export let tabDetails = {
+    CITY: document.querySelector('.city-tab2'),
+    TEMP: document.querySelector('.temperature-tab2 span'),
+    FEEL_LIKE: document.querySelector('.feel-like-tab2 span'),
+    WEATHER: document.querySelector('.weather-tab2 span'),
+    SUNRISE: document.querySelector('.sunrise-tab2 span'),
+    SUNSET: document.querySelector('.sunset-tab2 span')
+};

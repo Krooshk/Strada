cfe04424e04a1cb6456fdb9782aff8ddc8b5aaf3
Krooshk <kirill.Krush@mail.ru>
2022-11-29 13:02:38 +0300

    feat: add solve

--- exercise_with_class/index.js
+++ exercise_with_class/index.js
@@ -1,18 +1,41 @@
 class Storage {
-
-    constructor(name) {
+    constructor(name,storage=localStorage) {
         this.name = name;
-    }
+        this.storage=storage;
 
+    }
     get() {
-        localStorage.getItem(this.name);
+        return this.storage.getItem(this.name);
     }
     set(value) {
-        localStorage.setItem(this.name,value);
+        this.storage.setItem(this.name,value);
     }
+    clear(){
+        this.storage.removeItem(this.name);
+    }
+    isEmpty(){
+        return (this.storage.getItem(this.name) ?? true);
+    }
+}
+let personMan = new Storage("Иван",sessionStorage);
+let personWoman = new Storage("Таня");
+let personGranpa = new Storage("Павел");
 
+personMan.set(20);
+personWoman.set(35);
+personGranpa.set(60);
+personGranpa.clear();
+console.log(personGranpa.isEmpty());
+
+
+
+for(let i=0; i<localStorage.length; i++) {
+    let key = localStorage.key(i);
+    console.log(`${key}: ${localStorage.getItem(key)}`);
 }
 
-// Использование:
-let user = new User("Иван");
-user.sayHi();
+/*
+for(let i=0; i<sessionStorage.length; i++) {
+    let key = sessionStorage.key(i);
+    console.log(`${key}: ${sessionStorage.getItem(key)}`);
+}*/
